d7160f4adb8e13a29cda65de4f622ecde6f11822
ROBSON ANJOS <anjosrobson72@gmail.com>
2024-04-18 20:23:09 -0300

    docker files

--- src/config/connectionFactory.ts
+++ src/config/connectionFactory.ts
@@ -1,13 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 
 const database = new Sequelize({
-	database: process.env.DB_URL,
+	database: process.env.DB_NAME,
 	username: process.env.DB_USERNAME,
 	password: process.env.DB_PASSWORD,
 	host: process.env.DB_HOST,
 	port: parseInt(process.env.DB_PORT),
 	dialect: "postgres",
-	models: [__dirname + "/models"],
+	models: [__dirname + "/src/adapters/database/v1"],
 });
 
 export default {
